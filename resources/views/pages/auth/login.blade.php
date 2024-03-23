00f89c7d91243d31df87e20935a9e8cd5cb1e2b3
Jesus Calderin <jesus.c@servicepad.com>
2024-03-23 17:55:36 -0400

    Campo en login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -11,8 +11,9 @@
             </div>
         </div>
         <div class="card-body px-5 py-4">
-            <form action="">
-                <label for="emailInput" class="form-label">Correo electronico</label>
+            <form action="{{route('login')}}" method="POST">
+                @csrf
+                <label for="emailInput" class="form-label">Nombre de usuario</label>
                 <div class="input-group mb-3">
                     <span class="input-group-text">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -23,7 +24,7 @@
                                 d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z" />
                         </svg>
                     </span>
-                    <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+                    <input type="uasername" class="form-control" id="emailInput" placeholder="name@example.com" value="{{old('email')}}">
                 </div>
                 <label for="passwordInput" class="form-label">Contraseña</label>
                 <div class="input-group mb-3">
@@ -35,10 +36,10 @@
                             <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
                         </svg>
                     </span>
-                    <input type="password" class="form-control" id="passwordInput" placeholder="****************">
+                    <input type="password" class="form-control" id="passwordInput" placeholder="****************" value="{{old('password')}}">
                 </div>
                 <div class="d-flex justify-content-end mt-5">
-                    <button type="button" class="btn btn-primary ">Ingresar al sistema</button>
+                    <button type="submit" class="btn btn-primary ">Ingresar al sistema</button>
                 </div>
             </form>
         </div>
